be64918c8a6b671f19c83cf8e16a3f02e36ca2e9
mikemonari <mikemonari31@gmail.com>
2022-09-20 12:52:02 +0000

    ifelse

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+def islower(x):
+
+    list1 = []
+
+    for a in range(ord('a'), ord('z') + 1):
+
+        list1.append(chr(a))
+
+    if (x not in list1):
+
         return True
+
     else:
+
         return False
