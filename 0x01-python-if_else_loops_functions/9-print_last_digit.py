f263ed7eb43db88e0fd8b2e8f6a2892fbd1bfcdf
mikemonari <mikemonari31@gmail.com>
2022-09-20 15:05:33 +0000

    isupper

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    new = (number % 10)
-    print("{:d}".format(new, end=''))
+    if number < 0:
+        number = (-1 * number)
+    last = (number % 10)
+    print("{:d}".format(last), end="")
+    return last
