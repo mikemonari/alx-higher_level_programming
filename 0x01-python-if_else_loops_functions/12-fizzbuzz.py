e510b26b5516a156ce1bf516f620f9184ba29981
mikemonari <mikemonari31@gmail.com>
2022-09-20 15:57:56 +0000

    isupper

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,10 @@ def fizzbuzz():
     for num in range(1, 101):
         if (num % 3 == 0 and num % 5 == 0):
             print("{:s}".format("Fizzbuzz"), end=" ")
-            continue
+            
         elif (num % 3 == 0):
             print("{:s}".format("Fizz"), end=" ")
-            continue
+            
         elif (num % 5 == 0):
             print("{:s}".format("Buzz"), end=" ")
         else:
