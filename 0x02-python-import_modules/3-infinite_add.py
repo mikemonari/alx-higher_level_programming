e795db7b23496d4270f16e3b31428954a96d621b
mikemonari <mikemonari31@gmail.com>
2022-09-22 13:07:33 +0000

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-x = 1
-lis = []
-for arg in range(len(sys.argv)):
-    while (x < len(sys.argv)):
-        lis.append(int(sys.argv[x]))
-        x = x + 1
-print("{}".format(sum(lis)))
+if __name__ == "__main__":
+    import sys
+    x = 1
+    lis = []
+    for arg in range(len(sys.argv)):
+        while (x < len(sys.argv)):
+            lis.append(int(sys.argv[x]))
+            x = x + 1
+    print("{}".format(sum(lis)))
