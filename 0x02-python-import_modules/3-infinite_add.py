13c9f11e10f8e3e1c6f176d1d4f07183971ccd44
mikemonari <mikemonari31@gmail.com>
2022-09-22 13:03:31 +0000

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+x = 1
+lis = []
+for arg in range(len(sys.argv)):
+    while (x < len(sys.argv)):
+        lis.append(int(sys.argv[x]))
+        x = x + 1
+print("{}".format(sum(lis)))
