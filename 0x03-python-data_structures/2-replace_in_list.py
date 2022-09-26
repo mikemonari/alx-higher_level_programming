deb6f84a50d16a03ff84d1906b5ffe1a71956deb
mikemonari <mikemonari31@gmail.com>
2022-09-26 05:04:59 +0000

    structs

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name == "__main__":
+    def replace_in_list(my_list, idx, element):
+        if idx >= len(my_list) and idx < len(my_list):
+            return my_list
+        mylist[idx] = element
+        return my_list
