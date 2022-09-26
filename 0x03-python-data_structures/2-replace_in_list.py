b493d5caad9caced7a8f61edf8935fc18278379a
mikemonari <mikemonari31@gmail.com>
2022-09-26 05:08:29 +0000

    structs

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     def replace_in_list(my_list, idx, element):
-        if idx >= len(my_list) and idx < len(my_list):
+        if idx >= len(my_list) or idx < len(my_list):
             return my_list
         mylist[idx] = element
         return my_list
