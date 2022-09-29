2ffb53f466fecf2893b741d971f13977887df542
mikemonari <mikemonari31@gmail.com>
2022-09-29 09:44:47 +0000

    del key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for x in a_dictionary:
-        if key not in a_dictionary:
-            return a_dictionary
-        else:
-            del a_dictionary[key]
+    if key not in a_dictionary:
+        return a_dictionary
+    else:
+        del a_dictionary[key]
     return a_dictionary
