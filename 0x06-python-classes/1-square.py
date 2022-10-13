8c1316c050831d7fef379878c1178e2151ac4e29
mikemonari <mikemonari31@gmail.com>
2022-10-13 21:01:11 +0000

    classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    """Define a class Square"""
-    def __init__(self, size):
+    """Define a class Square
+    """
+    def __init__(self, size)
         """instantiation with size"""
         self.__size = size
